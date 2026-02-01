3df259a3116a37c4c9ae22d85428787f87c65331
Jolan Fernandes <155242260+JolanFernandes@users.noreply.github.com>
2026-02-01 16:27:19 +0530

    Update image element creation method

--- script.js
+++ script.js
@@ -75,8 +75,7 @@ function showMessage(response) {
     yesMessage.textContent = "See you on the 14th mi bb";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    const imageElement = document.createElement("image");
-    imageElement.src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/dance.gif";
 
     // Remove the "Yes" button
     document.getElementById("yesButton").remove();
